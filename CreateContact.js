766548c7e58d8d37447c5014a83634013dfa7796
Kunal <kunalsonawane730@gmail.com>
2022-12-25 18:02:03 +0530

    Add: creating contact in address book system with JS

--- CreateContact.js
+++ CreateContact.js
@@ -1,2 +1,42 @@
 //Printing a welcome note to console
 console.log("Welcome to address book system!");
+
+//Printing a welcome note to console
+console.log("Welcome to address book system!");
+
+/**
+ * Created a class for contacts as blue print
+ */
+class AddressBookContact {
+
+    constructor (firstName, lastName, address, city, state, zipCode, phoneNumber, email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zipCode = zipCode;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    /**
+     * Method to log the data to console
+     * gets all the arguments given while creating new contact
+     * @returns String formatted contact
+     */
+    toString() {
+        return "firstName="+this.firstName+" ,lastName="+this.lastName+
+        " ,address="+this.address+" ,city="+this.city+" ,state="+this.state+
+        " ,zip="+this.zipCode+" ,phoneNumber="+this.phoneNumber+" ,email="+this.email;
+    }
+}
+
+/**
+ * Created a new contact
+ */
+let addressBook = new AddressBookContact("Kunal", "Sonawane", "Residency Layout", 
+    "Jalgaon", "Maharashtra", 560058, "9876543210", "kunal@gmail.com");
+
+//Printing the added contact to console
+console.log(addressBook.toString());
